48f3e0b62118f1b6c693c3f1b3a3bed71514adf1
ZilchofNowhere <81156765+ZilchofNowhere@users.noreply.github.com>
2022-07-11 04:29:23 +0300

    Powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,25 @@
+# Imports
+Import-Module 'C:\tools\gsudo\Current\gsudoModule.psd1'
+
+# Aliases
+sal -name reboot -value restart-computer
+sal -name set-color -value "Set-PSReadLineOption -Colors"
+sal -name get-color -value Get-PSReadLineOption
+sal -name input -value Read-Host
+sal -name msedge -value MicrosoftEdge.exe
+$Apps = $env:ProgramFiles
+$Appsx86 = ${env:ProgramFiles(x86)}
+sal -name ls -value logo-ls
+sal -name adb -value "$env:LOCALAPPDATA\Android\Sdk\platform-tools\adb.exe"
+
+# Theme
+Set-PoshPrompt -Theme aliens
+
+# Syntax highlighting
+Set-PSReadLineOption -Colors @{ Parameter = 'Cyan' }
+Set-PSReadLineOption -Colors @{ String = 'DarkYellow' }
+Set-PSReadLineOption -Colors @{ Operator = 'DarkCyan' }
+Set-PSReadLineOption -Colors @{ ContinuationPrompt = 'Blue' }
+Set-PSReadLineOption -Colors @{ Variable = "Magenta"}
+
+# EOF
