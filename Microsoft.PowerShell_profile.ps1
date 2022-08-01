ad3b10d7b67c3bb40dbc9f860a26887beb3c7df6
ZilchofNowhere <81156765+ZilchofNowhere@users.noreply.github.com>
2022-08-01 23:52:52 +0000

    CMatrix added

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,6 +11,7 @@ $Apps = $env:ProgramFiles
 $Appsx86 = ${env:ProgramFiles(x86)}
 sal -name ls -value logo-ls
 sal -name adb -value "$env:LOCALAPPDATA\Android\Sdk\platform-tools\adb.exe"
+sal -name cmatrix -value "Invoke-Cmatrix"
 
 # Theme
 Set-PoshPrompt -Theme aliens
@@ -21,5 +22,10 @@ Set-PSReadLineOption -Colors @{ String = 'DarkYellow' }
 Set-PSReadLineOption -Colors @{ Operator = 'DarkCyan' }
 Set-PSReadLineOption -Colors @{ ContinuationPrompt = 'Blue' }
 Set-PSReadLineOption -Colors @{ Variable = "Magenta"}
+
+# Functions
+function Invoke-Cmatrix {
+	unimatrix -n -s 96 -l o  # install unimatrix via pip first
+}
 
 # EOF
