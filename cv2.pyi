3d333e6b02e658d92344e05db88ddb38c25639b3
ZilchofNowhere <81156765+ZilchofNowhere@users.noreply.github.com>
2023-02-07 14:55:17 +0300

    OpenCV Python Intellisense

--- cv2.pyi
+++ cv2.pyi
@@ -1,6 +1,9 @@
 # Python: 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)]
 # Library: cv2, version: 4.4.0
 # Module: cv2.cv2, version: 4.4.0
+
+# Put at $(python -c "import cv2, os; print(os.path.dirname(cv2.__file__))")/__init__.pyi
+
 import typing
 import builtins as _mod_builtins
 import cv2 as _mod_cv2
