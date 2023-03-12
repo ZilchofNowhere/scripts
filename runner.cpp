d5d6ebcf734114e13d09ff3f222cb79699eb85fd
ZilchofNowhere <81156765+ZilchofNowhere@users.noreply.github.com>
2023-03-13 00:32:39 +0300

    Crossplatform runner script

--- runner.cpp
+++ runner.cpp
@@ -1,28 +1,29 @@
-#include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, char* const argv[]){
-  if (argc < 3) {
-    printf("Too few arguments.\n");
+  if (argc < 2) {
+    printf("Enter a file name.\n");
     return 1;
   }
-  
-  string lang = argv[1];
-  string file = argv[2];
+
+  string file = argv[1];
+  string lang = file.substr(file.rfind('.') + 1);
   string cmd;
-    
-  if (lang == "cpp" || lang == "cp") {
-    cmd = "clang++ -std=c++20 " + file + ".cpp -o " + file + " && ./" + file;
+  file = file.substr(0, file.find('.'));
+
+  if (lang == "cpp") {
+    cmd = "clang++ -std=c++2b " + file + ".cpp -o " + file + " && ./" + file;
   }
-  
+
   if (lang == "c") {
     cmd = "clang " + file + ".c -o " + file + " && ./" + file;
   }
-  
+
   if (lang == "py") {
     cmd = "python3 " + file + ".py";
   }
-  
+
   if (lang == "js") {
     cmd = "node " + file + ".js";
   }
@@ -30,14 +31,14 @@ int main(int argc, char* const argv[]){
   if (lang == "kt") {
     cmd = "kotlinc " + file + ".kt -include-runtime -d " + file + ".jar && kotlin " + file + ".jar"; 
   }
-  
+
   if (lang == "java") {
     cmd = "java " + file + ".java";
   }
-  
+
   if (lang == "cs") {
     cmd = "msc " + file + ".cs && mono " + file + ".exe";
   }
-  
+
   system(cmd.c_str());
 }
